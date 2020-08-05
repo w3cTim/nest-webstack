4bfb8061a800d2f2fd7f95123697781aea781490
tim <luo.tian@live.cn>
2020-08-05 10:45:57 +0800

    :children_crossing:  关键包 使用 cdn，提高用户体验

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,3 +1,27 @@
 module.exports = {
+  configureWebpack: (config) => {
+    config.externals = {
+      vue: 'Vue',
+      'element-ui': 'ELEMENT',
+      'vue-router': 'VueRouter',
+    }
+  },
+  chainWebpack: (config) => {
+    const cdn = {
+      // 访问https://unpkg.com/element-ui/lib/theme-chalk/index.css获取最新版本
+      css: ['//unpkg.com/element-ui@2.4.5/lib/theme-chalk/index.css', 'http://at.alicdn.com/t/font_1949907_usyu3f16zc.css'],
+      js: [
+        '//unpkg.com/vue@2.6.11/dist/vue.min.js', // 访问https://unpkg.com/vue/dist/vue.min.js获取最新版本
+        '//unpkg.com/vue-router@3.2.0/dist/vue-router.min.js',
+        '//unpkg.com/element-ui@2.4.5/lib/index.js',
+      ],
+    }
+    // 如果使用多页面打包，使用vue inspect --plugins查看html是否在结果数组中
+    config.plugin('html').tap((args) => {
+      // html中添加cdn
+      args[0].cdn = cdn
+      return args
+    })
+  },
   publicPath: process.env.NODE_ENV === 'production' ? '././' : '/',
 }
