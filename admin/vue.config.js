644f0ee8d9574dca1f803a5601be601fec1d3471
tim <luo.tian@live.cn>
2020-08-05 10:58:36 +0800

    🚸 关键包 使用 cdn，提高用户体验

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -9,11 +9,11 @@ module.exports = {
   chainWebpack: (config) => {
     const cdn = {
       // 访问https://unpkg.com/element-ui/lib/theme-chalk/index.css获取最新版本
-      css: ['//unpkg.com/element-ui@2.4.5/lib/theme-chalk/index.css', 'http://at.alicdn.com/t/font_1949907_usyu3f16zc.css'],
+      css: ['//unpkg.com/element-ui@2.11.0/lib/theme-chalk/index.css', 'http://at.alicdn.com/t/font_1949907_usyu3f16zc.css'],
       js: [
         '//unpkg.com/vue@2.6.11/dist/vue.min.js', // 访问https://unpkg.com/vue/dist/vue.min.js获取最新版本
         '//unpkg.com/vue-router@3.2.0/dist/vue-router.min.js',
-        '//unpkg.com/element-ui@2.4.5/lib/index.js',
+        '//unpkg.com/element-ui@2.11.0/lib/index.js',
       ],
     }
     // 如果使用多页面打包，使用vue inspect --plugins查看html是否在结果数组中
